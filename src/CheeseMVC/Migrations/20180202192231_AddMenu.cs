dbd1ad7ae913927dfa6df762a37194305f6c92dc
Brian Warden <brianwarden85@gmail.com>
2018-03-29 20:56:40 -0400

    Modified AddItem functions

---
+++ src/CheeseMVC/Migrations/20180202192231_AddMenu.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CheeseMVC.Migrations
+{
+    public partial class AddMenu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CheeseMenus",
+                table: "CheeseMenus");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CheeseMenus_CheeseID",
+                table: "CheeseMenus");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CheeseMenus",
+                table: "CheeseMenus",
+                columns: new[] { "CheeseID", "MenuID" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CheeseMenus_MenuID",
+                table: "CheeseMenus",
+                column: "MenuID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CheeseMenus",
+                table: "CheeseMenus");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CheeseMenus_MenuID",
+                table: "CheeseMenus");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CheeseMenus",
+                table: "CheeseMenus",
+                columns: new[] { "MenuID", "CheeseID" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CheeseMenus_CheeseID",
+                table: "CheeseMenus",
+                column: "CheeseID");
+        }
+    }
+}
